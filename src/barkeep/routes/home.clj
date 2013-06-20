d40025d69818acb3fc0ba1147a5b541b9924f701
Robert Sanders <robert@zeevex.com>
2013-06-20 17:04:05 -0400

    actually use URL components to parameterize barcode generation

--- src/barkeep/routes/home.clj
+++ src/barkeep/routes/home.clj
@@ -11,12 +11,29 @@
 (defn about-page []
   (layout/render "about.html"))
 
-(defn barcode []
+(defn content-type-for-image [format]
+  (case format
+    "gif" "image/gif"
+    "png" "image/png"
+    "jpg" "image/jpeg"
+    "application/binary"))
+
+(defn barcode [value format options]
+  (let [options (into {:type "c128c" :height "200" :width "400"} options)
+        height  (Integer. (:height options))
+        width   (Integer. (:width options))]
   {:status  200
-   :headers {"Content-Type" "image/png"}
-   :body    (java.io.ByteArrayInputStream. (make-barcode-image "1234567891")) })
+   :headers {"Content-Type" (content-type-for-image format) }
+   :body    (java.io.ByteArrayInputStream.
+             (make-barcode-image value
+                                 :imageformat format
+                                 :height height
+                                 :width width
+                                 :type (:type options)))
+   }))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/barcode" [] (barcode))
+  (GET "/barcode/:value.:format" [value format & options]
+       (barcode value format options))
   (GET "/about" [] (about-page)))
